fa70c3fb009a9555209fb45d11108ca20b57e6de
ridwangunawan47 <ridwangunawan4791@gmail.com>
2022-10-31 08:41:59 +0700

    tugas uts web lanjut

---
+++ index.php
@@ -0,0 +1,319 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portofolio Ridwan Gunawan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <style>
+        .nav-item {
+            padding-right: 50px;
+        }
+
+        .carousel-item {
+            height: 100vh;
+        }
+
+        section#projek {
+            padding-top:50px;
+            padding-bottom:50px;
+        }
+
+        section#biodata{
+            padding-top:50px;
+            padding-bottom:50px;
+        }
+
+        section#lokasi{
+            padding-top:50px;
+            padding-bottom:20px;
+        }
+
+        p {
+            text-align:justify !important;
+        }
+        .carousel-caption{
+            bottom: 220px;
+            z-index: 2;
+            -webkit-text-stroke:1px black;
+            font-style: italic;
+        }
+
+    </style>
+  </head>
+  <body>
+    
+    <nav id="navbar-custom" class="navbar navbar-expand-lg fixed-top navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Ridwan Gunawan</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#biodata">Biodata</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#projek">Projek</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#lokasi">Lokasi</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#kontak">Kontak</a>
+            </li>
+            
+        </ul>
+        
+        </div>
+    </div>
+    </nav>
+
+    <!-- HOME -->
+    <section id="home">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="images/1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block fs-3">
+                    <p class="text-center">Di dunia yang berubah sangat cepat, satu-satunya strategi yang dijamin gagal adalah tidak mengambil resiko</p>
+                    <p  class="text-center"> – Mark Zuckerberg</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+            <img src="images/2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block fs-3">
+                    <p class="text-center">Jika ada suatu peluang penting, kamu harus mencobanya meskipun kemungkinan kamu akan gagal</p>
+                    <p  class="text-center"> – Elon Musk</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+            <img src="images/3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block fs-3">
+                    <p class="text-center">Saya percaya bahwa hampir setengah hal yang membedakan pengusaha sukses dengan mereka yang tidak hanyalah ketekunan</p>
+                    <p  class="text-center"> – Steve Jobs</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button> 
+        </div>
+    </section>
+    
+    <!-- BIODATA -->
+    <section id="biodata">
+        <div class="container">
+            <h1 class="text-center" style="padding-bottom:30px">Biodata</h1>
+            <div class="row" style="padding-bottom:50px">
+                <div class="col-md-6" data-aos="fade-right">
+                    <h3>Data Diri</h3>
+                    <table class="fs-3">
+                            <tr>
+                                <td>Nama</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Ridwan Gunawan</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Kelamin</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Laki - laki</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Agama</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Islam</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Cianjur</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Hobi</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Nonton</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Cita - Cita</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Programmer</td>
+                                <td></td>
+                            </tr>
+                    </table>
+                </div>
+                <div class="col-md-6" data-aos="fade-left">
+                    <img src="images/4.jpg" class="rounded border border-primary" width="100%"/>
+                </div>
+            </div>
+            <div class="row" style="padding-bottom:50px">
+                <div class="col-md-6" data-aos="fade-right">
+                    <img src="images/5.jpg" class="rounded border border-primary" width="100%"/>
+                </div>
+                <div class="col-md-6" data-aos="fade-left">
+                <h3>Data Mahasiswa</h3>
+                    <table class="fs-3">
+                            <tr>
+                                <td>Nama</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Ridwan Gunawan</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>NPM</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>5520120006</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Kelas</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>IF C 2020</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Fakultas</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Teknik</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Prodi</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Informatika</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Universitas</td>
+                                <td>&nbsp : &nbsp</td>
+                                <td>Suryakancana Cianjur</td>
+                                <td></td>
+                            </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- PROJEK -->
+    <section id="projek">
+        <div class="container" data-aos="fade-up">
+            <h1 class="text-center" style="padding-bottom:30px;">Projek</h1>
+            <p class="text-center">Dalam mengerjakan projek ini saya menggunakan Bootstrap, yang merupakan framework web development yang dirancang untuk mempercepat proses pengembangan web responsive dan mobile-first (memprioritaskan perangkat seluler). Boostrap berbasis HTML, CSS, dan JavaScript.</p>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="images/6.png" class="card-img-top"/>
+                            <p class="card-text">HTML (Hypertext Markup Language) adalah bahasa markup yang digunakan untuk membuat halaman website. Isinya terdiri dari berbagai kode yang dapat menyusun struktur suatu website.
+
+                            HTML terdiri dari kombinasi teks dan simbol yang disimpan dalam sebuah file. Dalam membuat file HTML, terdapat standar atau format khusus yang harus diikuti. Format tersebut telah tertuang dalam standar kode internasional atau ASCII (American Standard Code for Information Interchange). 
+
+                            Struktur kode HTML
+                            Dengan adanya HTML, pengguna dapat membuat atau menyusun heading, paragraf, gambar, link, dan lainnya supaya dapat dilihat banyak orang melalui halaman website. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="images/7.png" class="card-img-top"/>
+                            <p class="card-text">CSS adalah singkatan dari cascading style sheets, yang digunakan untuk menentukan tampilan dan format halaman website. Dengan CSS, Anda bisa mengatur jenis font, warna tulisan, dan latar belakang halaman.
+
+                            CSS digunakan bersama dengan bahasa markup untuk membangun sebuah website yang menarik dan berjalan dengan baik.
+
+                            CSS juga berguna untuk mengatasi keterbatasan HTML dalam mengatur format halaman website.
+
+                            Apabila hanya menggunakan HTML, Anda harus menulis tag untuk sebuah elemen HTML di semua halaman tersebut. 
+
+                            Dengan adanya CSS, Anda cukup menulis kode satu kali untuk sebuah elemen HTML untuk diterapkan ke semua halaman.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="images/8.png" class="card-img-top"/>
+                            <p class="card-text">JavaScript adalah bahasa pemrograman populer yang digunakan untuk membuat situs dengan konten website yang dinamis.
+
+                            Konten dinamis artinya konten dapat bergerak atau berubah di depan layar tanpa perlu mereload halaman. Misalnya saja, fitur slideshow foto, gambar animasi, pengisian poling, dan lainnya.  
+
+                            JavaScript sendiri sebenarnya biasanya dikolaborasikan dengan HTML dan CSS. Di mana belajar HTML adalah untuk membuat struktur website dan CSS adalah bahasa untuk merancang style halaman website. Lalu, JavaScript berperan menambahkan elemen interaktif untuk meningkatkan interaksi pengguna dengan website.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- LOKASI -->
+    <section id="lokasi" style="background-color:#262f3e;">
+        <div class="container-fluid">
+            <h1 class="text-center text-white" style="padding-bottom:30px ">Lokasi</h1>
+            <div class="row">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.590668884618!2d107.1310345144701!3d-6.81953599507124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6852f78f2e5dfd%3A0x79172173225d7685!2sJl.%20Oto%20Iskandardinata%20III%2C%20Pamoyanan%2C%20Kec.%20Cianjur%2C%20Kabupaten%20Cianjur%2C%20Jawa%20Barat%2043211!5e0!3m2!1sid!2sid!4v1667177292133!5m2!1sid!2sid" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+
+    <!-- KONTAK -->
+    <section id="kontak" style="background-color:#262f3e; height:200px;">
+        <div class="container">
+            <div class="row" style="padding-top:50px">
+                    <div class="col-md-6 text-white">
+                        <h1>Kontak</h1>
+                        <p class="text-justify">Anda dapat mengontak saya melalui alamat email : ridwan@gmail.com</p>
+                    </div>
+                    <div class="col-md-6">
+
+                    </div>
+            </div>
+        </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#262f3e" fill-opacity="1" d="M0,288L48,282.7C96,277,192,267,288,240C384,213,480,171,576,133.3C672,96,768,64,864,48C960,32,1056,32,1152,37.3C1248,43,1344,53,1392,58.7L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+    <div class="container-fluid" style="padding-top:15px">
+        <div class="row">
+            <p class="text-center">Created with <i class="bi bi-heart-fill"></i> by <a href="#biodata" class="text-black">Ridwan</a></p>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+        let navbar = document.getElementById('navbar-custom');
+
+        window.addEventListener('scroll', function(){
+            let scroll = window.scrollY;
+
+            if (scroll > 50) {
+                navbar.classList.remove('navbar-dark');
+                navbar.classList.add('bg-light');
+            } else {
+                navbar.classList.add('navbar-dark');
+                navbar.classList.remove('bg-light');
+            }
+        })
+    </script>
+  </body>
+</html>
